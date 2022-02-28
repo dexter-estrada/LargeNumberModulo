9309e0562931365eda9804efe2f3c7701bfb66a8
dexter-estrada <dexter.estrada99@gmail.com>
2022-02-28 15:45:47 -0800

    Added error checking

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/*
+ * This
+ * Created by Dexter Estrada (dexter.estrada99@gmail.com)
+ */
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Enter exactly 3 arguments as \"M N e\"");
+    }
+    int M, N, e;    // User defined variables
+
     return 0;
 }
