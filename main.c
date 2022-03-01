cefade6f56600973b9a60c947bf3be52de584d6c
dexter-estrada <dexter.estrada99@gmail.com>
2022-02-28 16:08:33 -0800

    Added libraries and variables

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 /*
- * This
+ * This calculates the remainder of a very large number
+ * when dividing it with another number.
+ * Used when encrypting M
  * Created by Dexter Estrada (dexter.estrada99@gmail.com)
  */
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 4) {
         fprintf(stderr, "Enter exactly 3 arguments as \"M N e\"");
+        return 2;
     }
-    int M, N, e;    // User defined variables
+    // User defined variables
+    double M = atoi(argv[1]);
+    int N = atoi(argv[2]);
+    double e = atoi(argv[3]);
+
+    unsigned long dividend = pow(M, e);
+    int remainder = dividend % N;
+
+    printf("%d", remainder);
 
     return 0;
 }
